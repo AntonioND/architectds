5c79fa367f9a5db121cf8a0df5c718494c52b214
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-05-31 18:21:28 +0100

    core: Support injecting GL2D sprite sets to ARM9 binaries

--- architectds/architectds.py
+++ architectds/architectds.py
@@ -633,6 +633,68 @@ class Arm9Binary(GenericArmBinary):
                 '\n'
             )
 
+    def add_gl2d_sprite_set(self, in_dir, in_path_grit, width=0, height=0, out_dir='gl2d'):
+        '''
+        This function takes as input a directory full with PNG files and
+        generates a combined texture from them. It is possible to specify the
+        dimensions of the combined texture. If they aren't specified, the tool
+        will try different sizes until one works.
+
+        It is also required to provide a ".grit" file to convert the final
+        combined texture to a DS format.
+        '''
+        # Get name of the directory from the directory path
+        atlas_name = get_file_name(in_dir)
+
+        base_out_dir = os.path.join(self.out_assets_path, out_dir)
+        full_out_dir = os.path.join(base_out_dir, atlas_name)
+
+        self.add_dir_target(full_out_dir)
+
+        in_files = gen_input_file_list(in_dir, ('.png'))
+
+        out_path_c = os.path.join(full_out_dir, atlas_name + '.c')
+        out_path_h = os.path.join(full_out_dir, atlas_name + '.h')
+
+        out_path_png = os.path.join(full_out_dir, atlas_name + '_texture.png')
+
+        in_files_paths = " ".join(in_files)
+
+        # This rule must depend on both the PNG files (in case they change) and
+        # the directory that contains them (in case new files are added).
+        self.print(
+            f'build {out_path_png} {out_path_c} {out_path_h} : squeezerw {in_dir} {in_files_paths} || {full_out_dir}\n'
+            f'  args = --width {width} --height {height} --allowRotations no '
+            f'--outputTexture {out_path_png} --outputBaseName {atlas_name} '
+            f'--outputH {out_path_h} --outputC {out_path_c} --verbose {in_dir}\n'
+            '\n'
+        )
+
+        # When grit runs, the ".grit" file must be in the same folder as the
+        # ".png" file and have the same name. It is needed to copy it where the
+        # final ".png" file with the atlas is generated.
+        out_path_grit_copy = os.path.join(full_out_dir, atlas_name + '_texture.grit')
+
+        self.print(
+            f'build {out_path_grit_copy} : copy {in_path_grit} || {full_out_dir}\n'
+            '\n'
+        )
+
+        out_path_grit = os.path.join(full_out_dir, atlas_name + '_texture_png')
+        out_path_png_c = os.path.join(full_out_dir, atlas_name + '_texture_png.c')
+        out_path_png_h = os.path.join(full_out_dir, atlas_name + '_texture_png.h')
+
+        self.print(
+            f'build {out_path_png_c} {out_path_png_h}: grit {out_path_png} {out_path_grit_copy} || {full_out_dir}\n'
+            f'  in_path_img = {out_path_png}\n'
+            f'  grit_out_path = {out_path_grit}\n'
+            f'  options = -ftc -W1\n'
+            '\n'
+        )
+
+        self.assets_c.extend([out_path_c, out_path_png_c])
+        self.assets_h.extend([out_path_h, out_path_png_h])
+
     def add_mmutil(self, in_dirs, name='soundbank', out_dir='maxmod'):
         '''
         This function gets as input a list of directories. It will look for
@@ -1810,11 +1872,12 @@ class NdsRom(GenericBinary):
             'CXX_TEAK = ${LLVM_TEAK_PATH}clang++\n'
             'LD_TEAK  = ${LLVM_TEAK_PATH}ld.lld\n'
             '\n'
-            'BIN2C    = ${BLOCKSDS}/tools/bin2c/bin2c\n'
-            'GRIT     = ${BLOCKSDS}/tools/grit/grit\n'
-            'MMUTIL   = ${BLOCKSDS}/tools/mmutil/mmutil\n'
-            'NDSTOOL  = ${BLOCKSDS}/tools/ndstool/ndstool\n'
-            'TEAKTOOL = ${BLOCKSDS}/tools/teaktool/teaktool\n'
+            'BIN2C     = ${BLOCKSDS}/tools/bin2c/bin2c\n'
+            'GRIT      = ${BLOCKSDS}/tools/grit/grit\n'
+            'SQUEEZERW = ${BLOCKSDS}/tools/squeezer/squeezerw\n'
+            'MMUTIL    = ${BLOCKSDS}/tools/mmutil/mmutil\n'
+            'NDSTOOL   = ${BLOCKSDS}/tools/ndstool/ndstool\n'
+            'TEAKTOOL  = ${BLOCKSDS}/tools/teaktool/teaktool\n'
             '\n'
         )
 
@@ -1894,6 +1957,9 @@ class NdsRom(GenericBinary):
             '\n'
             'rule grit\n'
             '  command = ${GRIT} ${in_path_img} ${options} -o ${grit_out_path}\n'
+            '\n'
+            'rule squeezerw\n'
+            '  command = ${SQUEEZERW} ${args}\n'
             '\n'
             'rule grit_nf_shared\n'
             '  command = ${GRIT} ${in_files_png} ${options}\n'
