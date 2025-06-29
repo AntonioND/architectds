d004060a0cfea01f900aa6fab635acd52eb15fd5
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-06-30 00:49:25 +0100

    core: Update version number to 0.4.0

--- architectds/architectds.py
+++ architectds/architectds.py
@@ -26,7 +26,7 @@ import json
 import os
 
 AUTHOR_STRING = 'Antonio Niño Díaz'
-VERSION_STRING = '0.3.1'
+VERSION_STRING = '0.4.0'
 
 BLOCKSDS = os.environ.get('BLOCKSDS', '/opt/blocksds/core')
 BLOCKSDSEXT = os.environ.get('BLOCKSDSEXT', '/opt/blocksds/external')
